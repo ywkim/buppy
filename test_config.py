ee66b50e34252ffe893bbc76b136b40ab8454706
Youngwook Kim <youngwook.kim@gmail.com>
2023-10-15 13:45:39 +0900

    refactor(test_config): enhance test setup and function readability

--- test_config.py
+++ test_config.py
@@ -1,8 +1,10 @@
+import configparser
 import os
 import unittest
-import configparser
 from unittest.mock import patch
+
 from main import load_config_from_env_vars
+
 
 class TestConfig(unittest.TestCase):
     """
@@ -18,30 +20,34 @@ class TestConfig(unittest.TestCase):
 
     def setUp(self) -> None:
         """Define test variables and set up the constants."""
-        self.config_structure =  {
+        self.config_structure = {
             "api": {
                 "openai_api_key",
                 "serpapi_api_key",
                 "slack_bot_token",
-                "slack_app_token"
+                "slack_app_token",
             },
             "settings": {
                 "chat_model",
                 "system_prompt",
                 "temperature",
-            }
+            },
         }
 
     def test_load_config_from_env_vars(self) -> None:
         """Test load_config_from_env_vars() function to verify if it correctly reads the environment variables."""
-        with patch.dict(os.environ, {
+        with patch.dict(
+            os.environ,
+            {
                 "OPENAI_API_KEY": "test",
                 "SERPAPI_API_KEY": "test",
                 "SLACK_BOT_TOKEN": "test",
                 "SLACK_APP_TOKEN": "test",
                 "CHAT_MODEL": "gpt-4",
                 "SYSTEM_PROMPT": "You are a helpful assistant.",
-                "TEMPERATURE": "0"}):
+                "TEMPERATURE": "0",
+            },
+        ):
             config = load_config_from_env_vars()
             self.assertConfigStructure(config)
 
