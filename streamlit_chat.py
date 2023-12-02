3f4e6b1639fda050a5efe5f7222b9aee69008ca6
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-02 23:31:57 +0900

    feat(streamlit_app): add Firebase Firestore client initialization with service account
    Implemented service account authentication for the Firebase Firestore client within the Streamlit app to enhance security and manageability of the Firestore access credentials. This change introduces a private method _initialize_firebase_client in the StreamlitAppConfig class which sets up the Firestore client with credentials from Streamlit secrets, enforcing proper authentication.

--- streamlit_chat.py
+++ streamlit_chat.py
@@ -6,6 +6,7 @@ from typing import Any
 
 import streamlit as st
 from google.cloud import firestore
+from google.oauth2 import service_account
 from langchain.schema import AIMessage, BaseMessage, HumanMessage
 
 from config.app_config import AppConfig, init_chat_model
@@ -24,6 +25,30 @@ class StreamlitAppConfig(AppConfig):
         """Loads configuration from Streamlit secrets."""
         self.config.read_dict(st.secrets)
 
+    def _initialize_firebase_client(self) -> firestore.Client:
+        """
+        Initializes and returns a Firebase Firestore client using the service account details from Streamlit secrets.
+
+        Returns:
+            firestore.Client: The initialized Firestore client.
+
+        Raises:
+            ValueError: If the required service account details are not provided in Streamlit secrets.
+        """
+        service_account_info = st.secrets.get("firebase_service_account")
+        if not service_account_info:
+            raise ValueError(
+                "Firebase service account details not found in Streamlit secrets."
+            )
+
+        # Create a service account credential object
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info
+        )
+
+        # Initialize and return the Firestore client with the credentials
+        return firestore.Client(credentials=credentials)
+
     def load_config_from_firebase(self, companion_id: str) -> None:
         """
         Load configuration from Firebase Firestore using the provided companion ID.
@@ -34,7 +59,7 @@ class StreamlitAppConfig(AppConfig):
         Raises:
             FileNotFoundError: If the companion document does not exist in Firebase.
         """
-        db = firestore.Client()
+        db = self._initialize_firebase_client()
         companion_ref = db.collection("Companions").document(companion_id)
         companion = companion_ref.get()
         if not companion.exists:
