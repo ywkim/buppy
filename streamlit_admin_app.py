5e4ecaed3b67cf73f52aa84b419625840e0f7faf
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-07 17:03:49 +0900

    feat(streamlit): add CSV file upload and parsing logic for admin app

--- streamlit_admin_app.py
+++ streamlit_admin_app.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import streamlit as st
 from typing import List, Dict, Optional
-import pandas as pd
-from config.app_config import StreamlitAppConfig
+import csv
+from config.streamlit_config import StreamlitAppConfig
 
 class StreamlitAdminApp:
     """
@@ -62,18 +62,35 @@ class StreamlitAdminApp:
         companions = companions_ref.stream()
         return [companion.id for companion in companions]
 
-    def load_prefix_messages_from_csv(self, file_path: str) -> List[Dict[str, str]]:
+    def load_prefix_messages_from_csv(self, uploaded_file) -> List[Dict[str, str]]:
         """
-        Loads prefix messages from a CSV file.
+        Loads prefix messages from an uploaded CSV file and returns them as a list of dictionaries.
 
         Args:
-            file_path (str): The path to the CSV file.
+            uploaded_file: The file-like object uploaded through Streamlit's file_uploader.
 
         Returns:
-            List[Dict[str, str]]: List of prefix messages with their roles and content.
+            List[Dict[str, str]]: A list of dictionaries representing the messages.
+
+        The CSV file should contain messages with their roles ('AI', 'Human', 'System')
+        and content. These roles are mapped to Firestore roles ('assistant', 'user', 'system').
         """
-        df = pd.read_csv(file_path)
-        return df.to_dict('records')
+        role_mappings = {"AI": "assistant", "Human": "user", "System": "system"}
+
+        messages: list[dict[str, str]] = []
+
+        reader = csv.reader(uploaded_file)
+        for row in reader:
+            role, content = row
+            if role not in role_mappings:
+                raise ValueError(
+                    f"Invalid role '{role}' in CSV file. Must be one of {list(role_mappings.keys())}."
+                )
+            firestore_role = role_mappings[role]
+            messages.append({"role": firestore_role, "content": content})
+
+        return messages
+
 
 def main():
     """
@@ -86,7 +103,9 @@ def main():
 
     admin_app = StreamlitAdminApp()
 
-    # Companion ID Selection
+    default_chat_models = ["gpt-4", "gpt-4-1106-preview", "gpt-3.5-turbo"]
+
+    # Companion ID selection and existing data pre-fill logic
     companion_ids = admin_app.get_companion_ids()
     new_companion_option = "Add New Companion"
     selected_companion_id = st.selectbox("Select Companion ID", [new_companion_option] + companion_ids)
@@ -96,26 +115,28 @@ def main():
     if selected_companion_id != new_companion_option:
         existing_data = admin_app.get_companion_data(selected_companion_id) or {}
 
-    # Input Fields for Companion Data
-    chat_model = st.selectbox("Chat Model", ["gpt-4", "gpt-4-1106-preview", "gpt-3.5-turbo"],
-                              index=chat_models.index(existing_data.get("chat_model", "gpt-4")))
+    # Dynamically adjust the chat_models list based on existing data
+    chat_models = list(set(default_chat_models + [existing_data.get("chat_model", "gpt-4")]))
+    chat_model = st.selectbox("Chat Model", chat_models, index=chat_models.index(existing_data.get("chat_model", "gpt-4")))
+
     system_prompt = st.text_area("System Prompt", value=existing_data.get("system_prompt", ""))
     temperature = st.number_input("Temperature", min_value=0.0, max_value=2.0, step=0.01,
                                   value=existing_data.get("temperature", 1.0))
 
     # CSV File Uploader for Prefix Messages
     uploaded_file = st.file_uploader("Upload CSV for Prefix Messages", type="csv")
-    prefix_messages = []
+    prefix_messages_content = []
     if uploaded_file is not None:
-        prefix_messages = admin_app.load_prefix_messages_from_csv(uploaded_file)
+        prefix_messages_content = admin_app.load_prefix_messages_from_csv(uploaded_file)
 
-    # Upload Button
+    # Companion data upload logic
     if st.button("Upload Data"):
         companion_data = {
             "chat_model": chat_model,
             "system_prompt": system_prompt,
             "temperature": temperature,
-            "prefix_messages": prefix_messages
+            "vision_enabled": False,
+            "prefix_messages_content": prefix_messages_content
         }
         companion_id_to_upload = selected_companion_id if selected_companion_id != new_companion_option else st.text_input("Enter New Companion ID")
         if companion_id_to_upload:
