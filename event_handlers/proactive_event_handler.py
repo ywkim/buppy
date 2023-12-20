14976663ff4ca13f9bafd2a2ada1300685c7f0b5
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-20 11:31:13 +0900

    refactor(event-handler): consolidate proactive messaging logic

--- event_handlers/proactive_event_handler.py
+++ event_handlers/proactive_event_handler.py
@@ -1,9 +1,79 @@
 from __future__ import annotations
-from google.cloud import firestore
-from celery import Celery
 
-from utils.proactive_messaging_utils import should_reschedule
-from celery_tasks.proactive_messaging_task import schedule_proactive_message
+from celery import Celery
+from google.cloud import firestore
+from google.cloud.firestore import Transaction
+
+from utils.proactive_messaging_utils import (
+    ProactiveMessagingContext,
+    calculate_next_schedule_time,
+    should_reschedule,
+)
+
+@firestore.transactional
+def update_proactive_messaging_settings(
+    transaction: Transaction,
+    celery_app: Celery,
+    context: ProactiveMessagingContext,
+    bot_ref: firestore.DocumentReference,
+) -> None:
+    """
+    Transactionally updates proactive messaging settings in Firestore and schedules a task.
+
+    Args:
+        transaction (Transaction): Firestore transaction.
+        celery_app (Celery): The Celery application instance.
+        context (ProactiveMessagingContext): Context for proactive messaging.
+        bot_ref (firestore.DocumentReference): Reference to the Firestore document for the bot.
+    """
+    bot_doc = bot_ref.get(transaction=transaction).to_dict()
+    current_task_id = bot_doc.get("current_task_id", None)
+
+    next_schedule_time = calculate_next_schedule_time(proactive_config)
+    task = celery_app.send_task(
+        'generate_and_send_proactive_message', args=[context], eta=next_schedule_time
+    )
+    try:
+        transaction.update(
+            bot_ref,
+            {
+                "proactive_messaging.current_task_id": task.id,
+                "proactive_messaging.last_scheduled": next_schedule_time.isoformat(),
+            },
+        )
+    except Exception as e:
+        celery_app.control.revoke(task.id)
+        raise e
+    finally:
+        revoke_existing_tasks(celery_app, current_task_id)
+
+def update_task_id_in_firestore(
+    db_client: firestore.Client,
+    bot_id: str,
+    task_id: str
+) -> None:
+    """
+    Updates the current task ID in Firestore for the given bot.
+
+    Args:
+        db_client (firestore.Client): Firestore client for database operations.
+        bot_id (str): The bot ID for which to update the task ID.
+        task_id (str): The new task ID to be stored.
+    """
+    bot_ref = db_client.collection("Bots").document(bot_id)
+    bot_ref.update({"proactive_messaging.current_task_id": task_id})
+
+def revoke_existing_tasks(celery_app: Celery, task_id: str) -> None:
+    """
+    Revokes the existing proactive messaging task in Celery.
+
+    Args:
+        celery_app (Celery): The Celery application instance.
+        task_id (str): The ID of the task to be revoked.
+    """
+    if task_id:
+        celery_app.control.revoke(task_id)
+
 
 def process_proactive_event(
     db: firestore.Client,
@@ -27,4 +97,5 @@ def process_proactive_event(
         if should_reschedule(
             event_data["oldValue"]["fields"]["proactive_messaging"], proactive_config
         ):
-            schedule_proactive_message(celery_app, bot_ref, proactive_config, bot_id)
+            transaction = firestore.Client().transaction()
+            update_proactive_messaging_settings(transaction, celery_app, bot_ref, proactive_config, bot_id)
--- tests/test_proactive_event_handler.py
+++ tests/test_proactive_event_handler.py
@@ -1,24 +1,61 @@
 from __future__ import annotations
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, MagicMock
 
-from google.cloud import firestore
+import mockfirestore
+from google.cloud.firestore import Transaction, DocumentReference
 from celery import Celery
 
 import event_handlers.proactive_event_handler as event_handler
 
 class TestProactiveEventHandler(unittest.TestCase):
     def setUp(self):
-        self.mock_db = Mock(spec=firestore.Client)
+        self.mock_db = mockfirestore.MockFirestore()
         self.mock_celery_app = Mock(spec=Celery)
+        self.mock_transaction = MagicMock(spec=Transaction)
+        self.mock_document_ref = Mock(spec=DocumentReference)
+        self.bot_id = "test_bot_id"
+        self.task_id = "test_task_id"
+        self.proactive_config = {"config_key": "config_value"}
         self.event_data = {
             "id": "test_bot_id",
             "oldValue": {"fields": {"proactive_messaging": {"interval_days": 1}}},
             "newValue": {"fields": {"proactive_messaging": {"interval_days": 2}}}
         }
 
-    @patch("event_handlers.proactive_event_handler.schedule_proactive_message")
-    def test_process_proactive_event(self, mock_schedule_message):
+    def test_update_proactive_messaging_settings(self):
+        """
+        Test the update_proactive_messaging_settings function to ensure it correctly handles Firestore transactions and updates.
+        """
+        event_handler.update_proactive_messaging_settings(
+            self.mock_db.transaction(),
+            self.mock_celery_app,
+            self.mock_document_ref,
+            self.proactive_config,
+            self.bot_id
+        )
+
+        # Check if Firestore transaction is initiated
+        self.mock_db.transaction.assert_called_once()
+
+        # Check if the Firestore document is updated within a transaction
+        self.mock_document_ref.update.assert_called()
+
+    def test_update_task_id_in_firestore(self):
+        # Setup mock for Firestore document
+        mock_doc_ref = self.mock_db.collection("Bots").document(self.bot_id)
+
+        # Call the function under test
+        event_handler.update_task_id_in_firestore(
+            self.mock_db, self.bot_id, self.task_id
+        )
+
+        # Assert the expected call
+        mock_doc_ref.update.assert_called_with(
+            {"proactive_messaging.current_task_id": self.task_id}
+        )
+
+    def test_process_proactive_event(self):
         """
         Test process_proactive_event function to ensure it correctly processes events and schedules tasks.
         """
