a9193afaa8eb97d10052c743f95ed50f82a4964e
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-25 14:27:33 +0900

    refactor(settings): remove unnecessary Optional typing from broker_url

--- config/settings/celery_settings.py
+++ config/settings/celery_settings.py
@@ -1,8 +1,5 @@
-# pylint: disable=consider-alternative-union-syntax
-from typing import Optional
-
 from pydantic_settings import BaseSettings
 
 
 class CelerySettings(BaseSettings):
-    broker_url: Optional[str] = "pyamqp://guest:guest@localhost//"
+    broker_url: str = "pyamqp://guest:guest@localhost//"
