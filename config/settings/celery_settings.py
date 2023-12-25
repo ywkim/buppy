0a9ebdfcfdb6a1672dfb52ab5a44bf8e9c1ec3bc
Youngwook Kim <youngwook.kim@gmail.com>
2023-12-25 14:31:02 +0900

    feat(settings): add Config class to CelerySettings for env prefix

--- config/settings/celery_settings.py
+++ config/settings/celery_settings.py
@@ -3,3 +3,6 @@ from pydantic_settings import BaseSettings
 
 class CelerySettings(BaseSettings):
     broker_url: str = "pyamqp://guest:guest@localhost//"
+
+    class Config:
+        env_prefix = "CELERY_"
